71575f55869f399fdd3beb7f1b41ad5b585b3d65
Maximiliano Minetto <maximinetto@gmail.com>
2021-06-05 11:14:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Refactoring
+
+## Seguimiento del libro de Martin Fowler
+
+Este repositorio sigue paso a paso los conceptos del libro con el código.
---
+++ test/index.js
@@ -0,0 +1,18 @@
+const invoices = require('../invoices.json');
+const plays = require('../plays.json');
+
+const program = require("../index");
+
+const assert = require('assert');
+describe('Program', function() {
+  describe('#invoice', function() {
+    it('should return string', function() {
+      let result = "";
+      for(let invoice of invoices){
+        result = program(invoice, plays);
+      }
+    
+      assert.strictEqual(result, "Statement from BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $580.00 (48 seats)\nAmount owed is $1,810.00\nYou earned 55 credits\n");
+    });
+  });
+});
